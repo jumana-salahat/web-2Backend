d6ba96947fd8dd535096731777b9ef143ce3817c
Adam Fatayer <fatayemadam@gmail.com>
2026-05-16 14:49:12 +0300

    Adding Backend

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -9,10 +9,11 @@ import verifyToken from "../middleware/verifyToken.js";
 
 const router = Router();
 
-router.post("/register", register);
+router.post("/register", register); //when user do register
 
-router.post("/login", login);
+router.post("/login", login);//when user do login
 
-router.post("/google", verifyToken, googleAuth);
+router.post("/google", verifyToken, googleAuth); //when user do google auth, the FE will send the token to BE,
+                                                 //  then BE will verify the token and get the user data from the token,
 
 export default router;
